3f2d52b929cf8b4a85395feb77ae2640efe6826f
Cheburov Petr <cheburechkapetr@gmail.com>
2024-04-07 10:12:24 +0500

    done

--- cogs/magic8ball.py
+++ cogs/magic8ball.py
@@ -12,11 +12,15 @@ class M8Ball(commands.Cog):
         print("8ball is ready")
 
     @commands.command(aliases=['8ball', 'шар'])
-    async def M8B(self, ctx, *, question):
-        with open("assets/command_files/8ballresponces.txt", encoding="utf-8") as f:
-            responces = f.readlines()
-            await ctx.send(f'Мой ответ - {random.choice(responces)}')
-
+    async def M8B(self, ctx, question):
+        print(question)
+        if question:
+            with open("assets/command_files/8ballresponces.txt", encoding="utf-8") as f:
+                responces = f.readlines()
+                await ctx.reply(f'Мой ответ - {random.choice(responces)}')
+            print(question)
+        elif len(question) == 0:
+            await ctx.reply('нахуй пошел')
 
 async def setup(client):
     await client.add_cog(M8Ball(client))
