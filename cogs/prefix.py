88f059a655e27ccb25312701a56716864254efaa
Cheburov Petr <cheburechkapetr@gmail.com>
2024-04-07 10:17:28 +0500

    added prefix

---
+++ cogs/prefix.py
@@ -0,0 +1,22 @@
+import discord
+from discord.ext import commands
+
+class Prefix(commands.Cog):
+    def __init__(self, client):
+        self.client = client
+
+    @commands.Cog.listener()
+    async def on_ready(self):
+        print("Prefix is ready")
+
+    @commands.command(aliases=['префикс'])
+    async def prefix(self, ctx, prefix):
+        if not prefix:
+            await ctx.send(f"Текущий префикс - {self.client.command_prefix}")
+        else:
+            self.client.command_prefix = prefix
+            await ctx.send(f"Префикс изменен на {prefix}")
+
+
+async def setup(client):
+    await client.add_cog(Prefix(client))
